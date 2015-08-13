11e8fd47cd4b19dcf7dde8913d49ad70d3982dae
ahuzenko <yellowwarrior@gmail.com>
2015-08-13 23:49:16 +0300

    task1

---
+++ ImagesTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImagesTable
+//
+//  Created by Admin on 13.08.15.
+//  Copyright (c) 2015 Sunny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
